e680ce9b1109548e8bfba0596e0e34c9328d62fb
anazaryan <ashotnazaryan45@gmail.com>
2023-02-19 20:03:38 +0100

    feat: initial skeleton

---
+++ src/components/Panel.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import styles from 'styles/Panel.module.scss';
+
+class Panel extends Component {
+  render() {
+    return (
+      <div className={styles.Panel}>
+        Panel
+      </div>
+    );
+  }
+}
+
+export default Panel; 

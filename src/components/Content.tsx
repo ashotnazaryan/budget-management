f70891a9f03d3b96530ed7a1c6ec0c514b65a2b7
anazaryan <ashotnazaryan45@gmail.com>
2023-02-22 19:24:53 +0100

    feat: create right context menu

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,11 +1,21 @@
 import { Component } from 'react';
+import { MENU_ITEMS } from 'shared/constants';
+import RightContextMenu from 'shared/components/RightContextMenu';
 import styles from 'styles/Content.module.scss';
 
 class Content extends Component {
+  private get areaId(): string {
+    return 'content';
+  }
+
+  private get menuItems(): string[] {
+    return MENU_ITEMS;
+  }
+
   render() {
     return (
-      <div className={styles.Content}>
-        Content
+      <div id={this.areaId} className={styles.Content}>
+        <RightContextMenu areaId={this.areaId} menuItems={this.menuItems} />
       </div>
     );
   }
---
+++ src/shared/components/RightContextMenu.tsx
@@ -0,0 +1,77 @@
+import { Component } from 'react';
+import styles from 'styles/RightContextMenu.module.scss';
+
+interface RightContextMenuProps {
+  menuItems: string[];
+  areaId: string;
+}
+
+interface RightContextMenuState {
+  left: string;
+  top: string;
+  showMenu: boolean;
+}
+
+class RightContextMenu extends Component<RightContextMenuProps, RightContextMenuState> {
+  state = {
+    left: '0px',
+    top: '0px',
+    showMenu: false
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.handleClick);
+    document.addEventListener('contextmenu', this.handleContextMenu);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClick);
+    document.removeEventListener('contextmenu', this.handleContextMenu);
+  }
+
+  handleClick = (): void => {
+    if (this.state.showMenu) {
+      this.setState({ showMenu: false });
+    };
+  }
+
+  handleContextMenu = (event: MouseEvent): void => {
+    if (this.props.areaId !== (event.target as HTMLElement).id) {
+      return;
+    }
+
+    event.preventDefault();
+
+    this.setState({
+      left: `${event.pageX}px`,
+      top: `${event.pageY}px`,
+      showMenu: true,
+    });
+  }
+
+
+  render() {
+    const { showMenu, left, top } = this.state;
+
+    if (showMenu) {
+      return (
+        <div
+          className={styles.Menu}
+          style={{
+            top,
+            left
+          }}
+        >
+          {
+            this.props.menuItems.map((item) => (
+              <div className={styles.MenuItem} key={item}>{item}</div>
+            ))
+          }
+        </div>
+      );
+    }
+    return null;
+  }
+}
+
+export default RightContextMenu;

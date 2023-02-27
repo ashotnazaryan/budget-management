276c7894d1c740911c974a2da71a80f726929ba1
anazaryan <ashotnazaryan45@gmail.com>
2023-02-27 17:23:28 +0100

    feat: enhance routes

---
+++ src/shared/constants/routes.ts
@@ -0,0 +1,12 @@
+import { AppRoute } from 'shared/models';
+
+export const ROUTES: AppRoute = {
+  home: {
+    name: 'Home',
+    path: '/'
+  },
+  about: {
+    name: 'About',
+    path: 'about',
+  }
+};
--- src/shared/models/index.ts
+++ src/shared/models/index.ts
@@ -1,2 +1,3 @@
 export * from './summary';
 export * from './currency';
+export * from './route';

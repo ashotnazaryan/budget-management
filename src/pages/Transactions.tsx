dfe9fe126cabc34877875e6a3b6d1e50f54bd3b4
ashotnazaryan <ashotnazaryan45@outlook.com>
2023-03-13 19:39:13 +0100

    feat: integrate getSummary, addTransaction (#30)
    Co-authored-by: anazaryan <ashotnazaryan45@gmail.com>

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -6,6 +6,7 @@ import { useAppSelector } from 'store';
 import { selectDefaultCurrency, selectSummary } from 'store/reducers';
 
 const Transactions: React.FC = () => {
+  // TODO: findByText, transactions are being gotten only in Dashboard
   const { transactions } = useAppSelector(selectSummary);
   const { symbol } = useAppSelector(selectDefaultCurrency);
 
@@ -14,8 +15,9 @@ const Transactions: React.FC = () => {
       <Typography variant='h5' sx={{ textAlign: 'center', marginBottom: 3 }}>Transactions</Typography>
       <Grid container rowSpacing={2}>
         {
-          transactions?.map(({ uuid, createdAt, name, amount }) => (
-            <Grid item container key={uuid} columnSpacing={2}>
+          transactions?.map(({ createdAt, name, amount }) => (
+            // TODO: fix the key
+            <Grid item container key={`${name}-${amount}`} columnSpacing={2}>
               <Grid item xs={3}>
                 <Typography>{name}</Typography>
               </Grid>
--- src/shared/models/summary.ts
+++ src/shared/models/summary.ts
@@ -1,11 +1,29 @@
 import { Category, CategoryType } from './category';
 
+// TODO: distinguish Summary, SummaryDTO and SummaryState
+export interface SummaryDTO {
+  incomes: number;
+  expenses: number;
+  balance: number;
+  transactions: TransactionDataDTO[];
+  categoryTransactions: TransactionDataDTO[];
+}
+
 export interface SummaryState {
   incomes: number;
   expenses: number;
   balance: number;
   transactions: TransactionData[];
   categoryTransactions: TransactionData[];
+}
+
+export interface TransactionDataDTO {
+  uuid: string;
+  categoryId: Category['id'];
+  type: CategoryType;
+  name: Category['name'];
+  amount: number;
+  createdAt: Date;
 }
 
 export interface TransactionData {

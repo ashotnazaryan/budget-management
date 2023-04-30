cae89a91bb5bcd53a3e9482c5590e46a8c5385d5
anazaryan <ashotnazaryan45@gmail.com>
2023-04-30 16:51:26 +0200

    feat: translate currencies

--- src/shared/constants/currencies.ts
+++ src/shared/constants/currencies.ts
@@ -4,26 +4,31 @@ export const CURRENCIES: Currency[] = [
   {
     iso: 'USD',
     name: 'US Dollar',
+    nameKey: 'CURRENCIES.USD',
     symbol: '$'
   },
   {
     iso: 'EUR',
     name: 'Euro',
+    nameKey: 'CURRENCIES.EUR',
     symbol: '€'
   },
   {
     iso: 'PLN',
     name: 'Polish Zloty',
+    nameKey: 'CURRENCIES.PLN',
     symbol: 'zł'
   },
   {
     iso: 'UAH',
     name: 'Ukrainian Hryvnia',
+    nameKey: 'CURRENCIES.UAH',
     symbol: '₴'
   },
   {
     iso: 'AMD',
     name: 'Armenian Dram',
+    nameKey: 'CURRENCIES.AMD',
     symbol: '֏'
   }
 ];
--- src/shared/models/setting.ts
+++ src/shared/models/setting.ts
@@ -5,6 +5,7 @@ import { Locale } from './locale';
 export interface Currency {
   iso: 'USD' | 'EUR' | 'PLN' | 'UAH' | 'AMD';
   name: 'US Dollar' | 'Euro' | 'Polish Zloty' | 'Ukrainian Hryvnia' | 'Armenian Dram';
+  nameKey: 'CURRENCIES.USD' | 'CURRENCIES.EUR' | 'CURRENCIES.PLN' | 'CURRENCIES.UAH' | 'CURRENCIES.AMD';
   symbol: '$' | '€' | 'zł' | '₴' | '֏';
 }
 

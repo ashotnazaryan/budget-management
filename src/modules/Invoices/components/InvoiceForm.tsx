a853c7aadcfbf1b62fd6408902a2953148f1b706
anazaryan <ashotnazaryan45@gmail.com>
2023-12-17 13:51:44 +0100

    feat: improve invoices

--- src/modules/Invoices/components/InvoiceForm.tsx
+++ src/modules/Invoices/components/InvoiceForm.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
-import { Controller, FormProvider, useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import Grid from '@mui/material/Grid';
 import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 import Typography from '@mui/material/Typography';
+import { SelectChangeEvent } from '@mui/material/Select';
+import Paper from '@mui/material/Paper';
 import { useTheme } from '@mui/material/styles';
 import { useTranslation } from 'core/i18n';
-import { Invoice, InvoiceField } from 'shared/models';
+import { Currency, Invoice, InvoiceField } from 'shared/models';
 import { invoiceHelper } from 'shared/helpers';
 import { useAppSelector } from 'store';
 import { CURRENCIES, POSITIVE_NUMERIC_REGEX } from 'shared/constants';
@@ -25,15 +27,26 @@ const InvoiceForm: React.FC<InvoiceFormProps> = ({ onSubmit }) => {
   const { t } = useTranslation();
   const helper = invoiceHelper();
   const { palette: { info: { contrastText } } } = useTheme();
+  const { defaultCurrency: { iso } } = useAppSelector(selectSettings);
   const regex = POSITIVE_NUMERIC_REGEX;
   const currencies = CURRENCIES;
-  const { defaultCurrency: { iso } } = useAppSelector(selectSettings);
+  const [currencyIso, setCurrencyIso] = React.useState<Currency['iso']>(iso);
+  const [vatIncluded, setVatIncluded] = React.useState<boolean>(false);
 
   const defaultValues: Partial<Invoice> = {
     title: '',
     salary: '',
     currencyIso: iso,
-    vatIncluded: false
+    vatIncluded: false,
+    sellerName: '',
+    sellerAddress: '',
+    sellerLocation: '',
+    sellerVatID: '',
+    sellerAccount: '',
+    buyerName: '',
+    buyerAddress: '',
+    buyerLocation: '',
+    buyerVatID: ''
   };
 
   const methods = useForm<Invoice>({
@@ -42,87 +55,226 @@ const InvoiceForm: React.FC<InvoiceFormProps> = ({ onSubmit }) => {
     defaultValues
   });
 
+  const { setValue, handleSubmit } = methods;
+
+  const handleCurrencyChange = (event: SelectChangeEvent<string>): void => {
+    const value = event.target.value as Currency['iso'];
+
+    setValue(InvoiceField.currencyIso, value);
+    setCurrencyIso(value);
+  };
+
+  const handleVatIncludedChange = (_: React.ChangeEvent<HTMLInputElement>, checked: boolean): void => {
+    setValue(InvoiceField.vatIncluded, checked);
+    setVatIncluded(checked);
+  };
+
+  const handleFormSubmit = (data: Invoice): void => {
+    onSubmit(data);
+  };
+
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
-        <Grid container gap={7}>
-          <Grid item xs={12}>
-            <FormInput
-              label={t('COMMON.TITLE')}
-              name={InvoiceField.title}
-              rules={{
-                required: {
-                  value: true,
-                  message: t(helper.title.required!.message)
-                }
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormInput
-              label={t('COMMON.SALARY')}
-              type='number'
-              name={InvoiceField.salary}
-              rules={{
-                required: {
-                  value: true,
-                  message: t(helper.salary.required!.message)
-                },
-                pattern: {
-                  value: regex,
-                  message: t(helper.salary.pattern!.message)
-                }
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormSelect
-              label={t('COMMON.CURRENCY')}
-              name={InvoiceField.currencyIso}
-              value={iso}
-              rules={{
-                required: {
-                  value: true,
-                  message: t(helper.currencyIso.required!.message)
-                }
-              }}
-            >
-              {currencies.map(({ iso, name, nameKey, symbol }) => (
-                <MenuItem value={iso} key={iso}>
-                  <CurrencyInfoItem currency={{ iso, symbol, name, nameKey }} />
-                </MenuItem>
-              ))}
-            </FormSelect>
-          </Grid>
-          <Grid item xs={12}>
-            <Controller
-              name={InvoiceField.vatIncluded}
-              control={methods.control}
-              defaultValue={defaultValues.vatIncluded}
-              render={({ field }) => (
-                <FormControlLabel
-                  control={
-                    <Switch
-                      checked={field.value}
-                      onChange={(e) => field.onChange(e.target.checked)}
-                      name={InvoiceField.vatIncluded}
-                    />
-                  }
-                  label={
-                    <Typography color={contrastText}>{t('INVOICES.INCLUDE_VAT')}</Typography>
-                  }
-                  labelPlacement='start'
-                  sx={{
-                    '&.MuiFormControlLabel-root': {
-                      margin: 0
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <Grid container gap={3}>
+          <Grid item container>
+            <Paper elevation={1} sx={{ paddingX: 3, paddingTop: 2, paddingBottom: 6, width: '100%' }}>
+              <Typography sx={{ marginBottom: 2 }}>{t('INVOICES.MAIN_INFO')}</Typography>
+              <Grid item container gap={7}>
+                <Grid item xs={12}>
+                  <FormInput
+                    label={t('COMMON.TITLE')}
+                    name={InvoiceField.title}
+                    rules={{
+                      required: {
+                        value: true,
+                        message: t(helper.title.required!.message)
+                      }
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormInput
+                    label={t('COMMON.SALARY')}
+                    type='number'
+                    name={InvoiceField.salary}
+                    rules={{
+                      required: {
+                        value: true,
+                        message: t(helper.salary.required!.message)
+                      },
+                      pattern: {
+                        value: regex,
+                        message: t(helper.salary.pattern!.message)
+                      }
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormSelect
+                    label={t('COMMON.CURRENCY')}
+                    name={InvoiceField.currencyIso}
+                    value={currencyIso || iso}
+                    onChange={handleCurrencyChange}
+                    rules={{
+                      required: {
+                        value: true,
+                        message: t(helper.currencyIso.required!.message)
+                      }
+                    }}
+                  >
+                    {currencies.map(({ iso, name, nameKey, symbol }) => (
+                      <MenuItem value={iso} key={iso}>
+                        <CurrencyInfoItem currency={{ iso, symbol, name, nameKey }} />
+                      </MenuItem>
+                    ))}
+                  </FormSelect>
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        checked={vatIncluded}
+                        onChange={handleVatIncludedChange}
+                      />}
+                    label={
+                      <Typography color={contrastText}>{t('INVOICES.INCLUDE_VAT')}</Typography>
                     }
-                  }}
-                />
-              )}
-            />
+                    labelPlacement='start'
+                    sx={{
+                      '&.MuiFormControlLabel-root': {
+                        margin: 0
+                      }
+                    }}
+                  />
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid item container>
+            <Paper elevation={1} sx={{ paddingX: 3, paddingTop: 2, paddingBottom: 7, width: '100%' }}>
+              <Typography sx={{ marginBottom: 2 }}>{t('INVOICES.SELLER_INFO')}</Typography>
+              <Grid item container gap={7}>
+                <Grid item xs={12}>
+                  <FormInput
+                    label={t('COMMON.NAME')}
+                    name={InvoiceField.sellerName}
+                    rules={{
+                      required: {
+                        value: true,
+                        message: t(helper.sellerName.required!.message)
+                      }
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormInput
+                    label={t('COMMON.ADDRESS')}
+                    name={InvoiceField.sellerAddress}
+                    rules={{
+                      required: {
+                        value: true,
+                        message: t(helper.sellerAddress.required!.message)
+                      }
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormInput
+                    label={t('COMMON.LOCATION')}
+                    name={InvoiceField.sellerLocation}
+                    rules={{
+                      required: {
+                        value: true,
+                        message: t(helper.sellerLocation.required!.message)
+                      }
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormInput
+                    label={t('COMMON.VAT_ID')}
+                    name={InvoiceField.sellerVatID}
+                    rules={{
+                      required: {
+                        value: true,
+                        message: t(helper.sellerVatID.required!.message)
+                      }
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormInput
+                    label={t('COMMON.ACCOUNT')}
+                    name={InvoiceField.sellerAccount}
+                    rules={{
+                      required: {
+                        value: true,
+                        message: t(helper.sellerAccount.required!.message)
+                      }
+                    }}
+                  />
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid item container>
+            <Paper elevation={1} sx={{ paddingX: 3, paddingTop: 2, paddingBottom: 7, width: '100%' }}>
+              <Typography sx={{ marginBottom: 2 }}>{t('INVOICES.BUYER_INFO')}</Typography>
+              <Grid item container gap={7}>
+                <Grid item xs={12}>
+                  <FormInput
+                    label={t('COMMON.NAME')}
+                    name={InvoiceField.buyerName}
+                    rules={{
+                      required: {
+                        value: true,
+                        message: t(helper.buyerName.required!.message)
+                      }
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormInput
+                    label={t('COMMON.ADDRESS')}
+                    name={InvoiceField.buyerAddress}
+                    rules={{
+                      required: {
+                        value: true,
+                        message: t(helper.buyerAddress.required!.message)
+                      }
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormInput
+                    label={t('COMMON.LOCATION')}
+                    name={InvoiceField.buyerLocation}
+                    rules={{
+                      required: {
+                        value: true,
+                        message: t(helper.buyerLocation.required!.message)
+                      }
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormInput
+                    label={t('COMMON.VAT_ID')}
+                    name={InvoiceField.buyerVatID}
+                    rules={{
+                      required: {
+                        value: true,
+                        message: t(helper.buyerVatID.required!.message)
+                      }
+                    }}
+                  />
+                </Grid>
+              </Grid>
+            </Paper>
           </Grid>
           <Grid item xs={12}>
-            <Button aria-label='Save invoice' fullWidth type='submit' variant='contained' onClick={methods.handleSubmit(onSubmit)}>
+            <Button aria-label='Save invoice' fullWidth type='submit' variant='contained' onClick={handleSubmit(handleFormSubmit)}>
               {t('COMMON.PREVIEW')}
             </Button>
           </Grid>
---
+++ src/modules/Invoices/pages/CreateEditInvoice.styles.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+import { PDFViewer } from '@react-pdf/renderer';
+
+export const StyledPDFViewer = styled(PDFViewer)`
+  min-height: calc(100vh - 100px);
+  width: 100%;
+`;
